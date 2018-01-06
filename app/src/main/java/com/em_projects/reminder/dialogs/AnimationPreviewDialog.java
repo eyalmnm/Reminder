98be2c181435cf0141592c52cd6542d1f2f16a2d
eyalmnm <eyal@em-projects.com>
2018-01-06 16:36:56 +0200

    Adding Animation Preview Dialog.

---
+++ app/src/main/java/com/em_projects/reminder/dialogs/AnimationPreviewDialog.java
@@ -0,0 +1,70 @@
+package com.em_projects.reminder.dialogs;
+
+import android.app.DialogFragment;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.em_projects.reminder.R;
+import com.em_projects.reminder.loaders.LoaderFromAssets;
+import com.em_projects.reminder.storage.db.DbConstants;
+import com.em_projects.reminder.ui.AnimationCreator;
+import com.em_projects.reminder.utils.StringUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by eyalmuchtar on 1/6/18.
+ */
+
+public class AnimationPreviewDialog extends DialogFragment implements View.OnClickListener {
+    private static final String TAG = "AnimationPreviewDialog";
+
+    private String animationName;
+
+    private ImageView animationPreviewImageView;
+    private Button okButton;
+    private AnimationDrawable animDrawable;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        animationName = (String) getArguments().get(DbConstants.EVENTS_ANIMATION_NAME);
+        if (true == StringUtils.isNullOrEmpty(animationName)) dismiss();
+        return inflater.inflate(R.layout.dialog_animation_priview, null);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        animationPreviewImageView = view.findViewById(R.id.animationPreviewImageView);
+        okButton = view.findViewById(R.id.okButton);
+        okButton.setOnClickListener(this);
+
+        ArrayList<Drawable> drawables = null;
+        try {
+            drawables = LoaderFromAssets.getImagesByDir(getActivity(), animationName);
+            animDrawable = AnimationCreator.createAnimationDrawable(drawables, 50, false);
+            AnimationCreator.playAnimation(animationPreviewImageView, animDrawable);
+        } catch (IOException e) {
+            Log.e(TAG, "onViewCreated", e);
+            Toast.makeText(getActivity(), "Failed to play animation", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        animDrawable.stop();
+        dismiss();
+    }
+}
---
+++ app/src/main/java/com/em_projects/reminder/utils/BundleUtils.java
@@ -0,0 +1,21 @@
+package com.em_projects.reminder.utils;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Set;
+
+/**
+ * Created by eyalmuchtar on 1/6/18.
+ */
+
+public class BundleUtils {
+    private static final String TAG = "BundleUtils";
+
+    public static void printContent(Bundle bundle) {
+        Set<String> keys = bundle.keySet();
+        for (String key: keys) {
+            Log.d(TAG, "key: " + key + " value: " + bundle.get(key));
+        }
+    }
+}
